15bb73fb38c2d2d29e27fe2c6558ebd43391fad2
JuliaMathias <juliamss@gmail.com>
2021-10-20 18:58:37 -0300

    chore: move files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import styles from "./App.module.scss";
 
+import { LoginBox } from "./components/LoginBox";
+import { MessageList } from "./components/MessageList";
+import { SendMessageForm } from "./components/SendMessageForm";
+
 export function App() {
   return (
     <main className={`${styles.contentWrapper}`}>
-      <h1>Hello World</h1>
+      <MessageList />
+      <SendMessageForm />
+      <LoginBox />
     </main>
   );
 }
