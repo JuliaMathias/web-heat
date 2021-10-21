5691450ad4a516f0dad6d2b0e7199dbf9972055f
JuliaMathias <juliamss@gmail.com>
2021-10-21 17:24:32 -0300

    feat: SendMessageForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import styles from "./App.module.scss";
-
 import { LoginBox } from "./components/LoginBox";
 import { MessageList } from "./components/MessageList";
 import { SendMessageForm } from "./components/SendMessageForm";
+import { useAuth } from "./hooks/useAuth";
 
 export function App() {
+  const { user } = useAuth();
+
   return (
-    <main className={`${styles.contentWrapper}`}>
-      <MessageList />
-      {/* <SendMessageForm /> */}
-      <LoginBox />
-    </main>
+    <div className="App">
+      <main
+        className={`${styles.contentWrapper} ${
+          !!user ? styles.contentSigned : ""
+        }`}
+      >
+        <MessageList />
+        {!!user ? <SendMessageForm /> : <LoginBox />}
+      </main>
+    </div>
   );
 }
